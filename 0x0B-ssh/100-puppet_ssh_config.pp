d5304ee062d3003321f0cd20f903583074b83203
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-07-09 15:28:05 +0100

    alx tasks

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
-# compiled catalog for ubuntu-xenial 
-ssh ubuntu@3.83.245.5
+file { '/home/your_username/.ssh':
+  ensure => directory,
+  mode   => '0700',
+}
+
+file { '/home/your_username/.ssh/config':
+  ensure => file,
+  mode   => '0644',
+  content => "
+Host your_server_alias
+    HostName 3.83.245.5  # Replace with your server's IP or hostname
+    User ubuntu          # Replace with your server's username
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+",
+}
