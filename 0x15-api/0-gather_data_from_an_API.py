331acfce5469934993b10abb4913706224ba4e88
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-07-29 16:44:24 +0100

    alx tasks

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,23 +3,24 @@
 import requests
 import sys
 
+
 def fetch_employee_todo_progress(employee_id):
     """ URL for fetching user data"""
     user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    
+
     """Fetch user data"""
     user_response = requests.get(user_url)
     if user_response.status_code != 200:
         print("Error fetching user data")
         return
-    
+
     """ Extract user data"""
     user_data = user_response.json()
     employee_name = user_data.get('name')
 
     # URL for fetching todos
     todos_url = f'https://jsonplaceholder.typicode.com/todos'
-    
+
     # Fetch todos data
     todos_response = requests.get(todos_url)
     if todos_response.status_code != 200:
@@ -30,19 +31,23 @@ def fetch_employee_todo_progress(employee_id):
     todos_data = todos_response.json()
 
     # Filter todos for the specific employee
-    employee_todos = [todo for todo in todos_data if todo['userId'] == employee_id]
+    employee_todos = [todo for todo in todos_data
+                      if todo['userId'] == employee_id]
 
     # Calculate completed and total tasks
     total_tasks = len(employee_todos)
-    completed_tasks = [todo for todo in employee_todos if todo['completed']]
+    completed_tasks = [todo for todo in employee_todos
+                       if todo['completed']]
     number_of_done_tasks = len(completed_tasks)
 
     # Output the employee's TODO list progress
-    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with
+          tasks({number_of_done_tasks}/{total_tasks}): ")
 
     # Output the titles of completed tasks
     for todo in completed_tasks:
         print(f"\t {todo['title']}")
+
 
 def main():
     # Check if an employee ID is provided as a command-line argument
@@ -57,6 +62,6 @@ def main():
     except ValueError:
         print("Invalid employee ID. Please enter an integer.")
 
+
 if __name__ == "__main__":
     main()
-
