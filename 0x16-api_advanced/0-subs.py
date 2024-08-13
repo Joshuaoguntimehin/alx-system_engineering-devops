490b4590e630ef1d86c7127e0f58fd50e5aac794
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-08-13 16:23:14 +0100

    alx tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,6 +2,7 @@
 """import statement """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Define the URL for the Reddit API endpoint"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
@@ -11,13 +12,14 @@ def number_of_subscribers(subreddit):
         response = requests.get(url, headers)
         response.raise_for_status()
         data = response.json()
-    
-        subscribers = data['data']['subscribers']    
+
+        subscribers = data['data']['subscribers']
         return subscribers
-    
+
     except requests.RequestException as e:
         pass
         return 0
+
 
 if __name__ == "__main__":
     subreddit = "python"  # Replace with your subreddit
